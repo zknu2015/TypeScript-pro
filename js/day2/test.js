0e890ffde63deefe10ea7fc455d5c101b6af3cd3
liqiangqiang <2536963991@qq.com>
2021-06-11 16:54:44 +0800

    接口

---
+++ js/day2/test.js
@@ -0,0 +1,12 @@
+"use strict";
+function createSquare(config) {
+    let newSquare = { color: "white", area: 100 };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+let mySquare = createSquare({ color: "black" });
